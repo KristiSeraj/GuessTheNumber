8f4a4f51216c03c1444e0f7fec69ae259725d513
KristiSeraj <serajkristi@gmail.com>
2023-10-03 18:50:54 +0200

    fixed smth

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Generate();
+        Generate(10);
     }
-    public static void Generate() {
+    public static void Generate(int maxNum) {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("--Guess the number game--");
-        int maxNum = 10;
         int randomNum = rand.nextInt(maxNum);
         System.out.println("I am thinking of a number between 1 and " + maxNum);
-        int attempts = 3;
+        int attempts = 4;
         for (int attempt = 1; attempt <= attempts; attempt++) {
             System.out.println("You have " + attempt + " attempts, enter your guess");
             int userGuess = scanner.nextInt();
@@ -21,9 +20,9 @@ public class Main {
                 System.out.println("You found the number! Congratulations!!!");
                 break;
             } else if (userGuess > randomNum) {
-                System.out.println("Your number is higher");
+                System.out.println("Your guess is higher");
             } else {
-                System.out.println("Your number is lower");
+                System.out.println("Your guess is lower");
             }
         }
         scanner.close();
